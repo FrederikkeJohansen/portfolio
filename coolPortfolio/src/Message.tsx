7b61f373e8e4390bd9dbc7195aa9b12ece141071
au666766 <202004750@post.au.dk>
2025-09-11 16:58:16 +0200

    Tailwind installed and configured and tested

--- coolPortfolio/src/Message.tsx
+++ coolPortfolio/src/Message.tsx
@@ -1,5 +1,9 @@
 function Message() {
-  return <h1>Hello, welcome to my portfolio!</h1>;
+  return (
+    <h1 className="text-white bg-amber-700 font-bold">
+      Hello, welcome to my portfolio!
+    </h1>
+  );
 }
 
 export default Message;
