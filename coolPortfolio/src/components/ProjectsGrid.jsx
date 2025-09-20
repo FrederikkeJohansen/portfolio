f5680aafc1f8b988cbe715b371ad6632dc26dd70
au666766 <202004750@post.au.dk>
2025-09-20 20:24:45 +0200

    Add ProjectsGrid component and update HomePage layout

---
+++ coolPortfolio/src/components/ProjectsGrid.jsx
@@ -0,0 +1,14 @@
+function ProjectsGrid() {
+  return (
+    <div className="grid grid-cols-2 gap-4 py-0 px-4">
+      <div className="border rounded">Project 1</div>
+      <div className="border rounded">Project 2</div>
+      <div className="border rounded">Project 3</div>
+      <div className="border rounded">Project 4</div>
+      <div className="border rounded">Project 5</div>
+      <div className=" border rounded">Project 6</div>
+    </div>
+  );
+}
+
+export default ProjectsGrid;
--- coolPortfolio/src/pages/HomePage.jsx
+++ coolPortfolio/src/pages/HomePage.jsx
@@ -1,6 +1,14 @@
 import Hero from "../components/Hero";
+import ProjectsGrid from "../components/ProjectsGrid";
+import Footer from "../components/Footer";
 function HomePage() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <ProjectsGrid />
+      <Footer />
+    </>
+  );
 }
 
 export default HomePage;
