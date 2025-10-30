9f4941f6287710a9cdc5fe1c3e3b19b4dff62799
Frederikke Johansen <frederikke.ek.johansen@gmail.com>
2025-10-30 15:10:46 +0100

    Highlight added

--- coolPortfolio/src/components/Hero.jsx
+++ coolPortfolio/src/components/Hero.jsx
@@ -12,7 +12,7 @@ function Hero() {
         Frederikke
       </h1>
       <p className="text-center font-extralight text-base sm:text-lg md:text-xl lg:text-xl max-w-2xl mx-auto">
-        Hi! - I'm a MSc graduate in IT Product Development from Aarhus University with interests in hardware, front-end development, and UI/UX design. While I'm still early
+        Hi! - I'm a MSc graduate in IT Product Development from Aarhus University with interests in <span className="highlighted-text">hardware, front-end development, and UI/UX design.</span> While I'm still early
         in my journey and building my skills, I'm encouraged to learn and
         improve.
       </p>
